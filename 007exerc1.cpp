076e7c8824f22ede61b0cbe75236330e7ae18617
Lasbernardes <lasbernardes@uesc.br>
2023-10-07 16:01:19 -0300

    v1 007exerc1.cpp

--- 007exerc1.cpp
+++ 007exerc1.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main(){
 
+    for(int flag,i=1;i<=100;i++,flag=0){
+        if((i%3)==0){
+            cout<<i;
+            cout<<"Fizz";
+            flag=1;
+            if((i%5)==0){
+                cout<<"Buzz";
+                flag=1;
+            }
+        }
+        if(flag){
+            cout<<endl;
+        }
+    }
 
     return 0;
 }
